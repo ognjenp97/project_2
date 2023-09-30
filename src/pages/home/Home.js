7b058b2ca3a8eb4ce75b4912f33637fd8a76dcc2
ognjenp97 <ognjenpljevaljcic@hotmail.rs>
2023-09-30 19:03:19 +0200

    Added email list component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -5,6 +5,7 @@ import Header from "../../components/header/Header";
 import Featured from "../../components/featured/Featured";
 import PropertyList from "../../components/propertyList/PropertyList";
 import FeaturedBooking from "../../components/featuredBooking/FeaturedBooking";
+import MailList from "../../components/mailList/MailList";
 
 const Home = () => {
   return (
@@ -17,6 +18,7 @@ const Home = () => {
         <PropertyList />
         <h1 className="homeTitle">Homes guests love</h1>
         <FeaturedBooking />
+        <MailList />
       </div>
     </div>
   );
