68268fde38e00b07505a3768f12d430a0ca1c5cd
ognjenp97 <ognjenpljevaljcic@hotmail.rs>
2023-11-26 19:59:55 +0100

    Added JSON Web Token for authenticating a user

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import authRoute from "./routes/auth.js";
 import usersRoute from "./routes/users.js";
 import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
+import cookieParser from "cookie-parser";
 const app = express();
 dotenv.config();
 
@@ -21,6 +22,7 @@ mongoose.connection.on("disconnected", () => {
   console.log("mongoDB disconnected!");
 });
 
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,19 @@
 import express from "express";
+import {
+  updateUser,
+  deleteUser,
+  getUser,
+  getUsers,
+} from "../controllers/user.js";
 
 const router = express.Router();
+
+router.put("/:id", updateUser);
+
+router.delete("/:id", deleteUser);
+
+router.get("/:id", getUser);
+
+router.get("/", getUsers);
 
 export default router;
